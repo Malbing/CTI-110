5444398076bcc39ed136a3ba88e4cd43adf9c962
Malbing <Matthew.setter@live.com>
2024-09-16 11:24:58 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+# 9/16/24
+# P1LAB1
+# Get input from the user and display output.
+
+# Get first name from the user
+first_name = input ("Enter your first name: ")
+
+# Get last name from the user
+last_name = input ("Enter your last name: ")
+
+# Display info to user
+print ("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
