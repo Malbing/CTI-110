74a0ad94f1fda3202282f95e6308dedd306a9bb4
Malbing <Matthew.setter@live.com>
2024-10-23 11:12:28 -0400

    Add files via upload

---
+++ next assignment.py
@@ -0,0 +1,29 @@
+# 
+# 10/23/2024
+# P3HW2
+# Calulate reg and OT pay for an employee
+
+'''
+Input: Get name from user as string
+Input: Get number of hours worked as an interget
+Input: Get users regular pay rate as float
+
+Output a dotted line and employee name
+
+
+If employee has overtime (hours worked > 40)
+Calulate OT hours (hours worked -40)
+Calulate OT pay rate (reg pay rate * 1.5)
+Determine reg hours worked - has 40
+Calulate pay for regular hours (40 hours worked * pay rate)
+Calulate OT pay (OT hours * OT pay rate))
+Calulate gross pay ( reg pay + OT pay)
+
+
+
+else (has no overtime)
+hours worked is the orignial value from beginning
+pay rate is sam from beginning
+OT hours is 0
+OT pay rate is 0
+Calulate pay for regular hours (40 hours worked * pay rate)
